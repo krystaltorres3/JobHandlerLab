2900994333e1a21038c2595aca34d5cec438a4c2
Fernando R. Guzman Casas <fernando.guzman2@upr.edu>
2018-08-29 15:03:51 -0400

    implement getYearlySalary method

--- src/Position.java
+++ src/Position.java
@@ -68,8 +68,12 @@ public class Position {
 	public double getYearlySalary() {
 		
 		//Add Code Here [You have to use If/Else to get graded]
-		
-		return 0; //Temporal Return
-		
+		if(jobType) {
+			return hSalary*40*4.5*12;
+		}
+		else{
+			return hSalary*20*4.5*12;
+		}
+				
 	}
 }
