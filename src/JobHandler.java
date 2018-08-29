f759a01f12a705ab6247f6fac8a5ae5e904f6ffd
Fernando R. Guzman Casas <fernando.guzman2@upr.edu>
2018-08-29 15:38:17 -0400

    termine

--- src/JobHandler.java
+++ src/JobHandler.java
@@ -77,6 +77,21 @@ public class JobHandler {
 		if(eRank <= 0) { return qualify;}
 		
 		//Add Code Here [You have to use Switch and If/Else to get graded]
+		switch(seniority) {
+		case 1:
+			eRank += 5;
+			break;
+		case 2:
+			eRank += 10;
+			break;
+		case 3:
+			eRank += 20;
+			break;
+		default:
+			break;
+		}
+		if(eRank >= jobRank || !jobType)
+			qualify = true;
 		
 		return qualify; 
 	}
